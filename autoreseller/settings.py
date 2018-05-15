60a5e99434f37cde9cbc73fdfe7bed0c3a3c68b0
root <root@vps478648.ovh.net>
2018-05-15 20:13:39 +0200

    deploy fixes

--- autoreseller/settings.py
+++ autoreseller/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@dkli20)8d+jw29w!0_^i%-j%1#@bzbq9k-hudr+kdjk(g-+vb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['autoreseller.polart.lt', 'www.autoreseller.polart.lt', 'localhost']
 
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGGING = {
     'version': 1,
